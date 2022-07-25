83d98cd3a3d92466d7d3d5be53c17339120d07f4
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-07-25 15:30:21 +0300

    Basics added.

---
+++ Lectures/01-Basics/hello_world/target/debug/deps/hello_world-f6592e4b1028c24f.d
@@ -0,0 +1,5 @@
+c:\Users\Kaan\Documents\Rust-Notes\01-Basics\hello_world\target\debug\deps\hello_world-f6592e4b1028c24f.rmeta: src\main.rs
+
+c:\Users\Kaan\Documents\Rust-Notes\01-Basics\hello_world\target\debug\deps\hello_world-f6592e4b1028c24f.d: src\main.rs
+
+src\main.rs:
