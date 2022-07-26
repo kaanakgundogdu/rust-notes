7de797c473662be065803676a04c8bfbb760df2b
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-07-26 13:07:28 +0300

    Revert "Files modified."
    This reverts commit 9e91a5331ffa9803e0153c1824b134abb3327ffc.

---
+++ Lectures/01-Basics/hello_world/target/debug/deps/hello_world-d206fe4f327bfc16.d
@@ -0,0 +1,5 @@
+C:\Users\Kaan\Documents\Rust-Notes\01-Basics\hello_world\target\debug\deps\hello_world-d206fe4f327bfc16.exe: src\main.rs
+
+C:\Users\Kaan\Documents\Rust-Notes\01-Basics\hello_world\target\debug\deps\hello_world-d206fe4f327bfc16.d: src\main.rs
+
+src\main.rs:
---
+++ Lectures/01-Basics/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\Kaan\Documents\GitHub\rust-notes\Lectures\01-Basics\hello_world\target\debug\hello_world.exe: C:\Users\Kaan\Documents\GitHub\rust-notes\Lectures\01-Basics\hello_world\src\main.rs
