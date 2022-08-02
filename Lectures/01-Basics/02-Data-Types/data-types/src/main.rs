fe76a92f0adf26ddd17b87c3c86fe19a54965dc8
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-08-02 14:43:05 +0300

    data types modified.

--- Lectures/01-Basics/02-Data-Types/data-types/src/main.rs
+++ Lectures/01-Basics/02-Data-Types/data-types/src/main.rs
@@ -3,6 +3,13 @@
 
 
 fn main() {
-    let a: u8 =123;
+    let a: u8 =123; // unmutable
     println!("a = {}",a);
+
+    let mut b: i8 =18;
+    println!("b = {}",b);
+
+    b=-55;
+    println!("b = {}",b);
+
 }
