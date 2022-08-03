24c0dd44ebd3cab1f1e7dbfc7cd5d07dc9909ad7
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-08-03 14:20:11 +0300

    Operators lect. modified.

--- Lectures/01-Types-and-variables/03-Operators/operators/src/main.rs
+++ Lectures/01-Types-and-variables/03-Operators/operators/src/main.rs
@@ -19,5 +19,13 @@ fn main() {
     println!("{}^pi is {}", fnum,fnum_to_pi);
 
     //bitwise
+    let c = 1 | 2; // | OR, & AND, ^ XOR, !NOR
+    // 1 OR 2 -> 01 OR 10 = 11 == 3
+    println!("1 | 2 is : {}",c);
+    let two_to_ten = 1<<10;
+    println!("2^10 is {}",two_to_ten);
+
+    let two_less_four = 2<4; //true
+    println!("{}",two_less_four);
 
 }
