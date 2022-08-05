a163d48fb69104f2759dac02211f1df56272cac7
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-08-05 13:54:41 +0300

    if statement lect. added.

--- Lectures/02-Control-Flow/01-If-Statement/if_statement/src/main.rs
+++ Lectures/02-Control-Flow/01-If-Statement/if_statement/src/main.rs
@@ -1,3 +1,30 @@
+
+fn if_statement(){
+    let temp=32;
+    if temp>30 {
+        println!("Its hot outside {} degre!",temp);        
+    }
+    else if temp<10 {
+        println!("Its really cold outside {} degre!",temp);
+    }
+    else {
+        println!("Temperature is ok. {} degre!",temp);
+    }
+    
+    let day= if temp>20 {"sunny"} else {"cloudy"};
+    println!("Today is {}",day);
+
+    println!("it is {} outside.",
+    if temp>20 {"hot"} else if temp<10 {"colod"} else {"OK"});
+
+    let new_temp=44;
+    println!("it is {}", 
+    if new_temp>30{
+        if new_temp>40{"very hot"} else {"hot"}
+    }
+    else if new_temp<10 {"colod"} else {"OK"});
+}
+
 fn main() {
-    println!("Hello, world!");
+    if_statement();
 }
